c95ed9b6b7a40f14e7894944b8db29f8552fb2df
Ayo-Show <sowunmi30@gmail.com>
2021-11-30 12:44:45 -0500

    Refactored feature to update database

--- routes/products.js
+++ routes/products.js
@@ -39,18 +39,18 @@ module.exports = (db) => {
     const name = req.body.name;
     const price = (req.body.price * 100);
     const image = req.body.image;
-    const description = req.body.description;
     const brand = req.body.brand;
-    const screenSize = req.body.screenSize;
+    const screenSize = req.body.screen-size;
     const colour = req.body.colour;
-    const operatingSystem = req.body.operatingSystem;
-    const processorType = req.body.processorType;
-    const hardDriveCapacity = req.body.hardDriveCapacity;
+    const operatingSystem = req.body.operating-system;
+    const hardDriveCapacity = req.body.hard-drive-capacity;
     const ram = req.body.ram;
     const userID = req.session.user_id;
+    const description = req.body.description;
+    const imageURL = req.body.image-url;
 
     const sql = `INSERT INTO products (seller_id, name, brand, price, screen_size, color, operating_system, hard_drive_capacity, RAM, description, image_url) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING *;`
-  db.query(sql, [userID, name, brand, price, screenSize, colour, operatingSystem, hardDriveCapacity, ram, description, image])
+  db.query(sql, [userID, name, brand, price, screenSize, colour, operatingSystem, hardDriveCapacity, ram, description, imageURL])
     .then(() => {
       res.redirect("/products")
     })
@@ -68,32 +68,4 @@ module.exports = (db) => {
 
 
 
-//post request to create new products//
 
-
-// router.post("/newProducts", (req, res) => {
-//   const email = req.body.email;
-//   const values = [email];
-//   const sqlQuery = `SELECT * FROM users WHERE email = $1`;
-//   db
-//     .query(sqlQuery, values)
-//     .then((data) => {
-//       const user = data.rows[0];
-//       if (!user) {
-//         const name = req.body.name;
-//         const email = req.body.email;
-//         const password = req.body.password;
-//         const values2 = [images of products, name, brand, screen size, colour, operating system, processor type, hard drive capacity, ram, price, description];
-//         const sqlQuery2 = `INSERT INTO products (images of products, name, brand, screen size, colour, operating system, processor type, hard drive capacity, ram, price, description) VALUES ($1, $2, $3)`;
-//         return db
-//         .query(sqlQuery2, values2)
-//         .then((data) => {
-//           res.redirect("/products");
-//         })
-
-//       }
-//     })
-//     .catch((err) => {
-//       res.status(500).json({ err: err.message });
-//     });
-// });
