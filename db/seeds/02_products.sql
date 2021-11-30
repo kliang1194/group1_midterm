d01261d626f7e8313016e5423c795e5b2a931351
Harry Jiang <harry10291993@hotmail.com>
2021-11-30 14:06:02 -0500

    Change price and screen size row to different datatypes

--- db/seeds/02_products.sql
+++ db/seeds/02_products.sql
@@ -1,5 +1,5 @@
 INSERT INTO products (seller_id, name, brand, price, screen_size, color, operating_system, hard_drive_capacity, RAM, is_available, is_featured, description, image_url)
-VALUES(1, 'MacBook Pro Red', 'Apple', 300.00, 13, 'Red', 'MacOS', '256GB', '8GB', 'true', 'false', 'A red MacBook', 'https://i.imgur.com/oKIOdKm.jpg'),
-(2, 'MacBook Pro Pink', 'Apple', 250.00, 13, 'Pink', 'MacOS', '128GB', '4GB', 'true', 'false', 'A pink MacBook', 'https://i.imgur.com/k2I0KZd.jpg'),
-(3, 'MacBook Pro SB', 'Apple', 500.00, 13, 'Sky blue', 'MacOS', '1TB', '16GB', 'true', 'false', 'A sky blue Macbook', 'https://i.imgur.com/joYVSHY.jpg'),
-(2, 'Alienware M15 R1 Gaming Laptop', 'Alienware', 2400.00, 15.6, 'White', 'Windows', '4TB', '16GB', 'true', 'true', 'New Alienware Gaming Laptop', 'https://i.imgur.com/S5pSmGT.jpg');
+VALUES(1, 'MacBook Pro Red', 'Apple', 300, '13 inch', 'Red', 'MacOS', '256GB', '8GB', 'true', 'false', 'A red MacBook', 'https://i.imgur.com/oKIOdKm.jpg'),
+(2, 'MacBook Pro Pink', 'Apple', 250, '13 inch', 'Pink', 'MacOS', '128GB', '4GB', 'true', 'false', 'A pink MacBook', 'https://i.imgur.com/k2I0KZd.jpg'),
+(3, 'MacBook Pro SB', 'Apple', 500, '13 inch', 'Sky blue', 'MacOS', '1TB', '16GB', 'true', 'false', 'A sky blue Macbook', 'https://i.imgur.com/joYVSHY.jpg'),
+(2, 'Alienware M15 R1 Gaming Laptop', 'Alienware', 2400, '16 inch', 'White', 'Windows', '4TB', '16GB', 'true', 'true', 'New Alienware Gaming Laptop', 'https://i.imgur.com/S5pSmGT.jpg');
