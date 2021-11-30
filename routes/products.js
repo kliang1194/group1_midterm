2ddadf5cedff3fd0560a378493dff235ad916ffa
Ayo-Show <sowunmi30@gmail.com>
2021-11-30 14:22:08 -0500

    created a single product page

--- routes/products.js
+++ routes/products.js
@@ -38,19 +38,18 @@ module.exports = (db) => {
   router.post("/newProducts", (req, res) => {
     const name = req.body.name;
     const price = (req.body.price * 100);
-    const image = req.body.image;
     const brand = req.body.brand;
-    const screenSize = req.body.screen-size;
-    const colour = req.body.colour;
-    const operatingSystem = req.body.operating-system;
-    const hardDriveCapacity = req.body.hard-drive-capacity;
-    const ram = req.body.ram;
+    const screenSize = req.body.screen_size;
+    const color = req.body.color;
+    const operatingSystem = req.body.operating_system;
+    const hardDriveCapacity = req.body.hard_drive_capacity;
+    const ram = req.body.RAM;
     const userID = req.session.user_id;
     const description = req.body.description;
-    const imageURL = req.body.image-url;
+    const imageURL = req.body.image_url;
 
-    const sql = `INSERT INTO products (seller_id, name, brand, price, screen_size, color, operating_system, hard_drive_capacity, RAM, description, image_url) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING *;`
-  db.query(sql, [userID, name, brand, price, screenSize, colour, operatingSystem, hardDriveCapacity, ram, description, imageURL])
+    const sql = `INSERT INTO products (seller_id, name, brand, price, screen_size, color, operating_system, hard_drive_capacity, RAM, description, image_url) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11);`
+  db.query(sql, [userID, name, brand, price, screenSize, color, operatingSystem, hardDriveCapacity, ram, description, imageURL])
     .then(() => {
       res.redirect("/products")
     })
@@ -59,7 +58,24 @@ module.exports = (db) => {
         .status(500)
         .json({ error: err.message });
     });
-  })
+  });
+
+
+  router.get("/:product_id", (req, res) => {
+    const user_email = req.session.user_email;
+    const is_admin = req.session.is_admin;
+    const user_id = req.session.user_id;
+    const product_id = req.params.product_id;
+    const sql = `
+    SELECT * FROM products
+    WHERE products.id = $1;`
+  db.query(sql, [product_id])
+    .then((data) => {
+      const product = data.rows[0];
+      const templateVars = {user_email, is_admin, user_id, product};
+res.render("productPage", templateVars);
+    })
+  });
 
   return router;
 };
