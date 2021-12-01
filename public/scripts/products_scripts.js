422b0d23b4f6879d171bfef4b8418d9269fbd0ff
Harry Jiang <harry10291993@hotmail.com>
2021-11-30 23:41:44 -0500

    Implement frontend of favorite button and add files for favorites route

--- public/scripts/products_scripts.js
+++ public/scripts/products_scripts.js
@@ -1,60 +1,23 @@
 $(document).ready(function() {
 
-  const renderProducts = function(products) {
-    // loops through products
-    for (const product of products) {
-      renderProduct(product);
-    }
-  };
+  $(function () {
+    $('[id*="favorite-button"]').submit(function(event) {
+      event.preventDefault();
 
-  // Helper function that renders a single product
-  const renderProduct = function(product) {
-    const $product = createProductElement(product);         // calls createProductElement for each product
-    $('#products-container').append($product);             // takes return value and appends it to the tweets container
-  };
+      const target_id = event.target.id;
+      $(`#${target_id}`).css("display", "none");
+      $(`#un${target_id}`).css("display", "inline-block");
+    })
 
-    /* Takes in product object and return a product <article> element */
-    const createProductElement = function(product) {
-      const $product = $(`
-      <article class="product">
-        <div class="image-content">
-          <img src="${product.image_url}" width="300" height="300">
-        </div>
+    $('[id*="unfavorite-button"]').submit(function(event) {
+      event.preventDefault();
 
-        <div class="product-content">
-          <div class="description-section">
-            <h3>${product.name}</h3>
-            <p>${product.description}</p>
-          </div>
+      const target_id = event.target.id;
+      const target_id_2 = target_id.replace("un", "");
+      $(`#${target_id}`).css("display", "none");
+      $(`#${target_id_2}`).css("display", "inline-block");
+    })
+  });
 
-          <div class="link-section">
-            <form method="GET" action="/products/<%= product_id %>">
-              <button type="submit" class="btn btn-primary">EDIT</button>
-            </form>
-            <a class="nav-item-nav-link" href="/products/${product.id}/:user_id/newFavorite">Favourite This</a>
-            <a class="nav-item-nav-link" href="/messages/${product.seller_id}">Contact Seller</a>
-          </div>
-        </div>
-      </article>
-      `);
-      return $product;
-    };
 
-  const loadProducts = function() {
-    $.ajax({
-      url: '/products/json',
-      method: 'GET',
-      dataType: 'json',
-      success: (data) => {
-        console.log('this request succeeded and here\'s the data', data);
-        renderProducts(data);
-      },
-      error: (error) => {
-        console.log('this request failed and this was the error', error);
-      }
-    });
-  };
-
-  // Test / driver code
-  loadProducts();
 });
--- routes/products.js
+++ routes/products.js
@@ -24,6 +24,34 @@ module.exports = (db) => {
       });
   });
 
+  router.post("/:product_id/:user_id/newFavorite", (req, res) => {
+    const is_admin = req.session.is_admin;
+    const current_user_id = req.session.user_id;
+    const user_id = req.params.user_id;
+
+    console.log(current_user_id);
+    console.log(user_id);
+
+    if(current_user_id.toString() !== user_id) {
+      return res.status(400).send("Invalid Favorite request! (User Favorite function)");
+    };
+
+    if(is_admin) {
+      return res.redirect('/admin/products');
+    };
+    
+    db.query(`SELECT * FROM products;`)
+      .then(data => {
+        // const products = data.rows;
+        // const templateVars = {is_admin, user_email, products, user_id};
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
    
   return router;
 };
