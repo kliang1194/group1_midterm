78e61503654efe13cd35344063c06781e9808901
Kyle <kliang1194@gmail.com>
2021-12-01 11:48:25 -0500

    Add functionality to contact seller from a product listing

--- routes/messages.js
+++ routes/messages.js
@@ -5,36 +5,62 @@ const router  = express.Router();
 module.exports = (db) => {
 
   //render page to send messages to other users//
-  router.get("/newMessage", (req, res) => {
-    const user_email = req.session.user_email;
+  router.get("/newMessage/:product_id/:seller_id", (req, res) => {
     const user_id = req.session.user_id;
-    const user_name = req.session.user_name;
+    const receiver_id = req.params.seller_id
+    const user_email = req.session.user_email;
     const is_admin = req.session.is_admin;
-    const value = [user_email];
-    const sqlQuery = `SELECT * FROM users WHERE email = $1;`
-    db.query(sqlQuery, value)
-    .then((data) => {
-        const templateVars = {is_admin, user_email, user_id, user_name};
-        return res.render("newMessage", templateVars);
+    const product_id = req.params.product_id
+  const value = [receiver_id, user_id]
+  const sqlQuery = `
+      SELECT m.*, us.name as sender_name, ur.name as receiver_name
+      FROM messages m
+      INNER JOIN users us on m.sender_id = us.id
+      INNER JOIN users ur on m.receiver_id = ur.id
+      WHERE (sender_id = $1 AND receiver_id = $2) OR (receiver_id = $1 AND sender_id = $2)
+      ORDER BY timestamp ASC;`
+      db.query(sqlQuery, value)
+      .then((data) => {
+        const messages = data.rows;
+          const input = [req.params.seller_id]
+          const productQuery = `
+          SELECT seller_id, users.name as seller_name
+          FROM products
+          INNER JOIN users ON seller_id = users.id
+          WHERE seller_id = $1;`
+          db.query(productQuery, input)
+          .then((data2) => {
+            const seller_name = data2.rows[0]["seller_name"];
+            console.log("seller name:", seller_name);
+            const templateVars = {user_id, receiver_id, product_id, user_email, is_admin, messages, seller_name};
+            res.render("newMessage", templateVars);
+          })
+        })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
       })
-    .catch((err) => {
-      res.status(500).json({error: err.message});
     })
-  });
 
   //post route to send messages to other users//
-  router.post("/newMessage", (req, res) => {
+  router.post("/newMessage/:product_id/:seller_id", (req, res) => {
     const user_id = req.session.user_id;
-    const receiver_id = req.body.receiver_id;
-    const product_id = req.body.product_id;
-    const is_for_admin = req.body.is_admin;
+    const receiver_id = req.params.seller_id;
+    const product_id = req.params.product_id
+    const is_for_admin = false;
     const content = req.body.text;
     const value = [user_id, receiver_id, product_id, content, is_for_admin]
     const sqlQuery = `INSERT INTO messages (sender_id, receiver_id, product_id, content, is_for_admin) VALUES ($1, $2, $3, $4, $5)`
     db.query(sqlQuery, value)
     .then(() => {
-     res.redirect("/login")
-    });
+     res.redirect(`/messages/${receiver_id}`)
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    })
     });
 
     //render page to send messages to the admimn//
@@ -57,6 +83,11 @@ module.exports = (db) => {
         const templateVars = {user_id, receiver_id, user_email, is_admin, messages};
         res.render("adminMessage", templateVars);
       })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      })
     })
 
     //post route to send messages from users to admins//
@@ -64,19 +95,22 @@ module.exports = (db) => {
       const user_id = req.session.user_id;
       const receiver_id = 1;
       const is_for_admin = true;
-      const user_email = req.body.user_email;
-      const user_name = req.body.user_name;
       const content = req.body.text;
       const value = [user_id, receiver_id, content, is_for_admin]
       const sqlQuery = `INSERT INTO messages (sender_id, receiver_id, content, is_for_admin) VALUES ($1, $2, $3, $4)`
       db.query(sqlQuery, value)
       .then(() => {
-       res.redirect("/login")
-      });
+       res.redirect(req.originalUrl)
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      })
       });
 
 
- //post route to send message from admin to user//
+ //post route to send message from user to user//
     router.post("/messages/:sender_id", (req, res) => {
       const user_id = req.session.user_id;
       const receiver_id = req.params.sender_id
@@ -86,10 +120,15 @@ module.exports = (db) => {
       db.query(sqlQuery, value)
       .then(() => {
        res.redirect(req.originalUrl)
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
       });
       });
 
-//render individual conversation with sender from admin//
+//render individual conversation with sender from user to admin or other user//
 router.get("/messages/:sender_id", (req, res) => {
   const user_id = req.session.user_id;
   const sender_id = req.params.sender_id;
@@ -111,6 +150,11 @@ router.get("/messages/:sender_id", (req, res) => {
     console.log(messages);
     const templateVars = {user_id, sender_id, user_email, is_admin, messages, user_name};
     res.render("respondUser", templateVars);
+  })
+  .catch(err => {
+    res
+      .status(500)
+      .json({ error: err.message });
   })
 } else if (user_email && !is_admin) {
   const value = [sender_id, user_id];
@@ -135,6 +179,11 @@ router.get("/messages/:sender_id", (req, res) => {
     const messages = data.rows;
     const templateVars = {user_id, sender_id, user_email, is_admin, messages, user_name};
     res.render("respondUser", templateVars);
+  })
+  .catch(err => {
+    res
+      .status(500)
+      .json({ error: err.message });
   })
 }
 });
